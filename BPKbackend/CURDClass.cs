6b662002d1d8304a1d2194a8dfbebd1ac7acc6b4
Rahul Srivastava <rahulsri221007@gmail.com>
2023-06-13 01:37:14 +0530

    Add project files.

---
+++ BPKbackend/CURDClass.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BPKbackend
+{
+    public class CURDClass
+    {
+        public string Firstname { get; set; }
+
+        public string Lastname { get; set; }
+
+        public string City { get; set; }
+
+        public string State { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public string DOB { get; set; }
+                
+    }
+}
---
+++ BPKbackend/Controllers/CURD.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BPKbackend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CURD : ControllerBase
+    {
+        static List<CURDClass> CURDClasses =new List<CURDClass>();
+        // GET: api/<CURD>
+        [HttpGet]
+        public IEnumerable<CURDClass> Get()
+        {
+            return CURDClasses;
+        }
+
+    
+
+        // POST api/<CURD>
+        [HttpPost]
+        public void Post([FromBody] CURDClass value)
+        {
+            CURDClasses.Add(value);
+        }
+
+        
+
+      
+      
+    }
+}
